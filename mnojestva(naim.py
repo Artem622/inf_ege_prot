0a191e65e7ece877f35c62ad2b27fea92cde4c51
Artem622 <60893489+Artem622@users.noreply.github.com>
2022-04-05 20:51:19 +0300

    Add files via upload

--- mnojestva(naim.py
+++ mnojestva(naim.py
@@ -1,9 +1,11 @@
 P=[2, 4, 6, 8, 10, 12]
-Q=[]
+Q=[3, 6, 9, 12, 15]
 def f(x,A):
-    return (x in A) <= (x**2 <= 81) and (y**2 in Q) <= (y in A):
+    return (x in P) <= (((x in Q) and not(x in A)) <= (not(x in P)))
 A=set()
 for x in range(1,10000):
     if not f(x,A):
         A.add(x)
 print(sum(sorted(A)))
+
+#(x ∈ {2, 4, 6, 8, 10, 12}) → (((x ∈ {3, 6, 9, 12, 15}) ∧ ¬(x ∈ A)) → ¬(x ∈ {2, 4, 6, 8, 10, 12}))
